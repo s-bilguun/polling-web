92bf18657a5d8d5f4738f12f8c0da67d3b185e55
Undercroft0 <seekndestroy0501@gmail.com>
2023-06-09 10:00:21 +0800

    Poll/feed index.js styling

--- pages/index.js
+++ pages/index.js
@@ -6,38 +6,36 @@ import Header from './Header';
 const Page = () => {
   const isLoggedIn = true; // Change this based on your authentication logic
   const polls = [
-    { id: 1, title: 'Таны дуртай өнгө юу вэ?', username: 'User 1', startDatetime: '2023-06-06', endDatetime: '2023-06-10' },
-    { id: 2, title: 'Poll 2', username: 'User 2', startDatetime: '2023-06-07', endDatetime: '2023-06-11' },
-    { id: 3, title: 'Poll 3', username: 'User 3', startDatetime: '2023-06-08', endDatetime: '2023-06-12' },
+    { id: 1, title: 'Таны дуртай өнгө юу вэ?', username: 'User 1', startDatetime: '2023-06-06 05:12:00', endDatetime: '2023-06-10 05:12:00' },
+    { id: 2, title: 'Poll 2', username: 'User 2', startDatetime: '2023-06-07 05:12:00', endDatetime: '023-06-11 05:12:00' },
+    { id: 3, title: 'Poll 3', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
   ];
 
   return (
     <div>
       <Header />
 
-      {isLoggedIn && (
-        <Link href="/poll_create">
-          <button className="create-poll-button">
-            Create New Poll
-          </button>
-        </Link>
-      )}
 
       <div className="poll-list">
-        <h2>Poll Feed</h2>
+  <h2>Poll Feed</h2>
 
-        {polls.map(poll => (
-          <div key={poll.id} className="poll-item">
-             <Link href={`/poll/${poll.id}`} passHref>
-              <div className="poll-title-link">{poll.title}</div>
-            </Link>
-            <p className="poll-username">Username: {poll.username}</p>
-            <p className="poll-datetime">Start Datetime: {poll.startDatetime}</p>
-            <p className="poll-datetime">End Datetime: {poll.endDatetime}</p>
-           
-          </div>
-        ))}
+  {polls.map(poll => (
+    <div key={poll.id} className="poll-item">
+      <div className="poll-details">
+        <div className="poll-username">Username: {poll.username}</div>
+        <div className="poll-title-link">
+          <Link href={`/poll/${poll.id}`} passHref>
+            {poll.title}
+          </Link>
+        </div>
       </div>
+      <div className="poll-datetime">
+        <p>Start Datetime: {poll.startDatetime}</p>
+        <p>End Datetime: {poll.endDatetime}</p>
+      </div>
+    </div>
+  ))}
+</div>
 
       {/* ... rest of your code */}
     </div>
