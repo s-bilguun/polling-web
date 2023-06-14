b21eb5bff91dbfde2fbf56b0aa849b34ce4cd6cc
Undercroft0 <seekndestroy0501@gmail.com>
2023-06-14 16:03:23 +0800

    create poll error when going to create poll without logging in

--- pages/poll_create.js
+++ pages/poll_create.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { AuthContext } from './AuthContext';
 
 const AddPoll = () => {
-  const { user ,setUser} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   const [question, setQuestion] = useState('');
   const [startDateTime, setStartDateTime] = useState(new Date().toISOString().slice(0, -8)); // Set default to current time
@@ -27,33 +27,39 @@ const AddPoll = () => {
   const handlePollSubmit = async (e) => {
     e.preventDefault();
 
+    // Check if the user is logged in
+    if (!user) {
+      console.log('You are not logged in');
+      return;
+    }
+
     // Format the date and time strings
     const startDateTimeFormatted = `${startDateTime}:00`;
     const endDateTimeFormatted = `${endDateTime}:00`;
 
     try {
       // Submit the poll data to the backend
-      const response = await axios.post('http://localhost:8001/poll/createPoll', {
-        // userid: user.id,
-        question,
-        startdate: startDateTimeFormatted,
-        expiredate: endDateTimeFormatted,
-        answer: choices,
-      },   {headers: {
-        'Authorization': `Bearer ${user.token}`,
-      },
-    });
+      const response = await axios.post(
+        'http://localhost:8001/poll/createPoll',
+        {
+          question,
+          startdate: startDateTimeFormatted,
+          expiredate: endDateTimeFormatted,
+          answer: choices,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
 
-    
-      
       // Reset the form
       setQuestion('');
       setStartDateTime('');
       setEndDateTime('');
       setChoices(['', '']);
-      console.log('User:', user);
-      console.log('User ID:', user.id);
-      
+
       // Go back to the index page or any other desired page
       router.push('/');
     } catch (error) {
@@ -65,52 +71,56 @@ const AddPoll = () => {
     <div className="card">
       <Header />
       <h1>Add Poll</h1>
-      <form onSubmit={handlePollSubmit}>
-        <label>
-          Question:
-          <input
-            type="text"
-            value={question}
-            onChange={(e) => setQuestion(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Start Date & Time:
-          <input
-            type="datetime-local"
-            value={startDateTime}
-            onChange={(e) => setStartDateTime(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          End Date & Time:
-          <input
-            type="datetime-local"
-            value={endDateTime}
-            onChange={(e) => setEndDateTime(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Choices:
-          {choices.map((choice, index) => (
-            <div key={index}>
-              <input
-                type="text"
-                value={choice}
-                onChange={(e) => handleChoiceChange(index, e.target.value)}
-                required
-              />
-            </div>
-          ))}
-          <button type="button" onClick={handleAddChoice}>
-            Add Choice
-          </button>
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+      {user ? (
+        <form onSubmit={handlePollSubmit}>
+          <label>
+            Question:
+            <input
+              type="text"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Start Date & Time:
+            <input
+              type="datetime-local"
+              value={startDateTime}
+              onChange={(e) => setStartDateTime(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            End Date & Time:
+            <input
+              type="datetime-local"
+              value={endDateTime}
+              onChange={(e) => setEndDateTime(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Choices:
+            {choices.map((choice, index) => (
+              <div key={index}>
+                <input
+                  type="text"
+                  value={choice}
+                  onChange={(e) => handleChoiceChange(index, e.target.value)}
+                  required
+                />
+              </div>
+            ))}
+            <button type="button" onClick={handleAddChoice}>
+              Add Choice
+            </button>
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <p>You are not logged in</p>
+      )}
     </div>
   );
 };
