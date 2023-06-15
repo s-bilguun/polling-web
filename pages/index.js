4b0264372b7e27a95b0841e6df219e787af54e12
Zvkkaaa <zvkkaaa@gmail.com>
2023-06-15 10:21:57 +0800

    userid changed username

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,46 @@ import { useRouter } from 'next/router';
 import Header from './Header';
 import SearchBar from './SearchBar';
 import DropdownSort from './DropdownSort';
+import {useState,useEffect} from "react";
+// const Page = () => {
+//   const router = useRouter();
+//   const isLoggedIn = true;
+// }
 
 const Page = () => {
-  const isLoggedIn = true; // Change this based on your authentication logic
+  // const router = useRouter();
+  // const isLoggedIn = true; 
+
+  // const handleLogin = (e) => {
+  //   e.preventDefault();
+  //   axios({
+  //     url: "http://localhost:8001/poll/list",
+  //     method: "GET",
+  //     headers: {},
+  //     data: {
+  //       pollid: pollid,
+  //       userid: userid,
+  //       question: question,
+  //       startdate: startdate,
+  //       expiredate: expiredate,
+  //     },
+  //   })
+  //     .then((res) => {
+  //       console.log("Connected to pollweb 2023");
+  //       login(res.data.token); // call the login function from AuthContext
+  //       router.push("/");
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       setErrorMessage("Login failed. Please check email or password"); // Set error message
+  //       console.log(err);
+  //     });
+
+  //   console.log("Login submitted:", { email, password });
+  //   setEmail("");
+  //   setPassword("");
+  // };
+  // Change this based on your authentication logic
   const polls = [
     { id: 1, title: 'Таны дуртай өнгө юу вэ?', username: 'User 1', startDatetime: '2023-06-06 05:12:00', endDatetime: '2023-06-10 05:12:00' },
     { id: 2, title: 'Чи юунаас хамгийн их айдаг вэ?', username: 'User 2', startDatetime: '2023-06-07 05:12:00', endDatetime: '023-06-11 05:12:00' },
@@ -23,6 +60,9 @@ const Page = () => {
     { id: 12, title: 'Poll 12', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
     
   ];
+// useEffect(()=>{
+//   fetchData();
+// },[]);
 
   const sortOptions = [
     { label: 'New polls', value: 'new polls' },
@@ -104,5 +144,22 @@ const Page = () => {
     </div>
   );
 };
-
+//  const fetchData = async () =>{
+//   try{
+//     const response = await axios.get('http://localhost:8001/poll/list');
+//     setDatasets(response.data);
+//   }catch(error){
+//     console.error('error at fetching:', error);
+//   }
+//   return (
+//     <div>
+//       <ul>
+//         {data.map((item)=>(
+//           <li key = {item.id}>{item.question}</li>
+//         ))}
+//       </ul>
+//     </div>
+//    );
+//  };
+ 
 export default Page;
