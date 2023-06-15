ed748fabeb6dfacb89eb341ba5e13ba35b09851b
temuujin <temvvjint27@gmail.com>
2023-06-15 11:15:35 +0800

    poll list fetch

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 // Page.js
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Header from './Header';
@@ -7,53 +7,29 @@ import SearchBar from './SearchBar';
 import DropdownSort from './DropdownSort';
 
 const Page = () => {
-  const isLoggedIn = true; // Change this based on your authentication logic
-  const polls = [
-    { id: 1, title: 'Таны дуртай өнгө юу вэ?', username: 'User 1', startDatetime: '2023-06-06 05:12:00', endDatetime: '2023-06-10 05:12:00' },
-    { id: 2, title: 'Чи юунаас хамгийн их айдаг вэ?', username: 'User 2', startDatetime: '2023-06-07 05:12:00', endDatetime: '023-06-11 05:12:00' },
-    { id: 3, title: 'Өнөө орой яахийй ?', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
-    { id: 4, title: 'Таны дуртай хоол юу вэ?', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
-    { id: 5, title: 'Та ямар утас барьдаг вэ?', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
-    { id: 6, title: 'Пиззанд дуртай юу?', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
-    { id: 7, title: 'Өглөө хэдэн цагт босч байна вэ?', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
-    { id: 8, title: 'Poll 8', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
-    { id: 9, title: 'Poll 9', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
-    { id: 10, title: 'Poll 10', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
-    { id: 11, title: 'Poll 11', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
-    { id: 12, title: 'Poll 12', username: 'User 3', startDatetime: '2023-06-08 05:12:00', endDatetime: '2023-06-12 05:12:00' },
-    
-  ];
+  
+ 
+  const [polls, setPolls] = useState([]);
 
+  useEffect(() => {
+    const fetchPolls = async () => {
+      try {
+        const response = await fetch('http://localhost:8001/poll/list');
 
-  const [pollData, setPollData] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          setPolls(data);
+        } else {
+          console.error('Failed to fetch polls');
+        }
+      } catch (error) {
+        console.error('Error fetching polls:', error);
+      }
+    };
 
-  const handlePoll = (e) => {
-    e.preventDefault();
-
-    setIsLoading(true);
-
-    axios
-      .get("http://localhost:8001/poll/list", {
-        params: {
-          id: 1, // replace with the actual value
-          userid: 1, // replace with the actual value
-          tquestion: "Sample question", // replace with the actual value
-          startDatetime: "2023-06-14", // replace with the actual value
-          endDatetime: "2023-06-14", // replace with the actual value
-        },
-      })
-      .then((res) => {
-        setPollData(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }
-
+    fetchPolls();
+  }, []);
 
   const sortOptions = [
     { label: 'New polls', value: 'new polls' },
@@ -101,25 +77,18 @@ const Page = () => {
             Sort by <DropdownSort options={sortOptions} onSelectSort={handleSort} />
           </div>
         </div>
-
-        <div>
-          <form onSubmit={handlePoll}>
-            <button type="submit">Fetch Poll</button>
-          </form>
-
-          {isLoading ? (
-            <p>Loading...</p>
-          ) : (
+        
+        {/* {getPollsForPage(currentPage).map((pollData) => (
+        <div key={pollData.id} className="poll-item">
             <div>
-              {/* Render the received pollData */}
               <p>ID: {pollData.id}</p>
               <p>User ID: {pollData.userid}</p>
-              <p>Question: {pollData.tquestion}</p>
+              <p>Question: {pollData.question}</p>
               <p>Start Date Time: {pollData.startDatetime}</p>
               <p>End Date Time: {pollData.endDatetime}</p>
             </div>
-          )}
           </div>
+        ))} */}
 
           {getPollsForPage(currentPage).map((poll) => (
             <div key={poll.id} className="poll-item">
@@ -127,7 +96,7 @@ const Page = () => {
                 <div className="poll-username">Username: {poll.username}</div>
                 <div className="poll-title-link">
                   <Link href={`/poll/${poll.id}`} passHref>
-                    {poll.title}
+                    {poll.question}
                   </Link>
                 </div>
               </div>
